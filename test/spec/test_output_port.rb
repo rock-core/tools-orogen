0f70e6b57d5c990e928a03a4cad1fc7da1fa28b8
eduardacoppo <eduarda.coppo@tidewise.io>
2025-02-06 08:56:25 -0300

    test: add tests for recommend_init flag

--- test/spec/test_output_port.rb
+++ test/spec/test_output_port.rb
@@ -15,6 +15,20 @@ module OroGen
             it "returns false in input?" do
                 refute @port.input?
             end
+
+            it "defaults recommend_init to true" do
+                assert @port.recommend_init
+            end
+
+            it "allows recommend_init to be explicitly set to false" do
+                @port.recommend_init = false
+                refute @port.recommend_init
+            end
+
+            it "sets recommend_init when recommends_init is called" do
+                @port.recommend_init
+                assert @port.recommend_init
+            end
         end
     end
 end
